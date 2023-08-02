84447a3936a06f04f59e873c740a6a1a447f29be
Girotto <l.girottoph@gmail.com>
2023-08-02 16:56:54 +0100

    tutorials done

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,9 +5,22 @@ extends Node2D
 func _ready() -> void:
 	Global.set_level(get_tree().current_scene.scene_file_path)
 	MusicController.play_main_music()
+	
+	$GUI.visible = false
+	$Tutorial.visible = true
+	$Tutorial/player.play("Task")
+	$"Tutorial/Got it".grab_focus()
+	
+	get_tree().paused = true
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("restart"):
 		get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_got_it_pressed():
+	$Tutorial.visible = false
+	$GUI.visible = true
+	
+	get_tree().paused = false
